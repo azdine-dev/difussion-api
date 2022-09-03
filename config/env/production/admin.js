da80df039ef52a15431ba1db93d5011e1b1e8796
azdine-dev <azeddinebenhalima@gmail.com>
2022-09-03 17:13:06 +0100

    add auth admin js

---
+++ config/env/production/admin.js
@@ -0,0 +1,9 @@
+const crypto = require("crypto");
+module.exports = ({ env }) => ({
+  auth: {
+    secret: crypto.randomBytes(16).toString("base64"),
+  },
+  apiToken: {
+    salt: crypto.randomBytes(16).toString("base64"),
+  },
+});
