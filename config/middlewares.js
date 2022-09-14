65b5fdafb29200f69303b79764c900e1199e39f0
azdine-dev <azeddinebenhalima@gmail.com>
2022-09-14 16:12:03 +0100

    change contenst security policy

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,8 +7,26 @@ module.exports = [
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com", // cloudinary images
+            "lh3.googleusercontent.com", // google avatars
+            "platform-lookaside.fbsbx.com", // facebook avatars
+            "dl.airtable.com", // strapi marketplace
+            `https://${env("AWS_BUCKET")}.s3.${env(
+              "AWS_REGION"
+            )}.amazonaws.com`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `https://${env("AWS_BUCKET")}.s3.${env(
+              "AWS_REGION"
+            )}.amazonaws.com`,
+          ],
           upgradeInsecureRequests: null,
         },
       },
